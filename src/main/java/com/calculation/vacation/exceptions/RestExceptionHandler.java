c5c80ea102975b5014eedec306a7463d9edc7c85
Lyub1m1y <dokhoyan-leva@mail.ru>
2023-05-14 19:36:23 +0300

    Added RestExceptionHandler

---
+++ src/main/java/com/calculation/vacation/exceptions/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.calculation.vacation.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+}
